40603c879e3272b84628071d3fd42346fd122a27
mekylei-belchior <mekylei@msn.com>
2021-09-17 12:43:36 -0300

    Implementado configurações para gerar o build .War

--- api/src/main/java/br/com/mekylei/contato/ContatoApplication.java
+++ api/src/main/java/br/com/mekylei/contato/ContatoApplication.java
@@ -2,9 +2,10 @@ package br.com.mekylei.contato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContatoApplication {
+public class ContatoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ContatoApplication.class, args);
